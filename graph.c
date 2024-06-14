241de4b408c2e4a64e3d24364baf7a8baa240cba
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-14 09:31:21 -0300

    fear: create_graph function.

--- graph.c
+++ graph.c
@@ -7,3 +7,16 @@
 #include <stdio.h>
 
 
+t_graph * create_graph(int vertices) {
+    t_graph * graph = (t_graph *) malloc(sizeof(t_graph));
+    graph->num_vertices = vertices;
+    graph->adj_matrix = (int **) malloc(sizeof(int *) * vertices);
+    for (int i = 0; i < vertices; ++i) {
+        graph->adj_matrix[i] = (int *) malloc(sizeof(int) * vertices);
+        for (int j = 0; j < vertices; ++j) {
+            graph->adj_matrix[i][j] = 0;
+        }
+    }
+    return graph;
+}
+
